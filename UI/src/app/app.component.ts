3159c7ed42fe268b39f97b5ad457e32e04fa06c6
Patrick Browning <patrick@browninglogic.com>
2018-10-14 14:55:03 -0400

    Display identity & access token claims

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthenticationService } from 'src/services/authentication.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bl-is-sample-ui';
+  title = 'IdentityServer Demo Client';
   constructor(private authenticationService: AuthenticationService) {}
 
   public get userAuthenticated() : boolean {
@@ -16,5 +16,17 @@ export class AppComponent {
 
   public logIn() {
     this.authenticationService.initImplicitFlow();
+  }
+
+  public logOut() {
+    this.authenticationService.logOut();
+  }
+
+  public get identityTokenClaims() : Object {
+    return this.authenticationService.idTokenClaims;
+  }
+
+  public get accessTokenClaims() : Object {
+    return this.authenticationService.accessTokenClaims;
   }
 }
