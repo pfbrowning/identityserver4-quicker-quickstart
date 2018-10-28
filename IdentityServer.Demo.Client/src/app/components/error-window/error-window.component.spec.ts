c7cb5a97107ce732d2d132efe09b7e9db305371f
Patrick Browning <patrick@browninglogic.com>
2018-10-28 19:10:01 -0400

    Provide basic 'should show modal' test

--- IdentityServer.Demo.Client/src/app/components/error-window/error-window.component.spec.ts
+++ IdentityServer.Demo.Client/src/app/components/error-window/error-window.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorWindowComponent } from './error-window.component';
-import { ModalManagerModule } from '@browninglogic/ng-modal';
+import { ModalManagerModule, ModalWindowComponent } from '@browninglogic/ng-modal';
+import { ErrorHandlingService } from '../../services/error-handling.service';
+import { By } from '@angular/platform-browser';
 
 describe('ErrorWindowComponent', () => {
   let component: ErrorWindowComponent;
   let fixture: ComponentFixture<ErrorWindowComponent>;
+  let errorHandlingService: ErrorHandlingService;
+  let errorModal: ModalWindowComponent;
+  // let errorMessageElement: any;
+  // let errorCommentElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [ ModalManagerModule ],
-      declarations: [ ErrorWindowComponent ]
+      declarations: [ ErrorWindowComponent ],
+      providers: [ ErrorHandlingService ]
     })
     .compileComponents();
   }));
@@ -18,10 +25,25 @@ describe('ErrorWindowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorWindowComponent);
     component = fixture.componentInstance;
+    errorHandlingService = fixture.debugElement.injector.get(ErrorHandlingService);
+    errorModal = component.errorModal;
+    //let errorMessageElement = fixture.debugElement.query(By.css('.errorMessage'));
+    //console.log('element', errorMessageElement);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show modal on error', () => {
+    expect(errorModal.visible).toBe(false);
+
+    errorHandlingService.handleError(new Error("Test Error"), "Test Message");
+
+    expect(errorModal.visible).toBe(true);
+    expect(component.appError.error['message']).toBe("Test Error");
+    expect(component.appError.comment).toBe("Test Message");
+    //expect(errorMessageElement.innerText).toBe("Test Error");
+  })
 });
