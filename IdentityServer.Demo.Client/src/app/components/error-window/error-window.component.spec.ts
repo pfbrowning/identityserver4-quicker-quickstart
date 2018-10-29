176993434b974fec9dcb08c71a3a17a61e7fe2a0
Patrick Browning <patrick@browninglogic.com>
2018-10-28 22:44:31 -0400

    Check model-bound properties on error window display

--- IdentityServer.Demo.Client/src/app/components/error-window/error-window.component.spec.ts
+++ IdentityServer.Demo.Client/src/app/components/error-window/error-window.component.spec.ts
@@ -4,14 +4,13 @@ import { ErrorWindowComponent } from './error-window.component';
 import { ModalManagerModule, ModalWindowComponent } from '@browninglogic/ng-modal';
 import { ErrorHandlingService } from '../../services/error-handling.service';
 import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('ErrorWindowComponent', () => {
   let component: ErrorWindowComponent;
   let fixture: ComponentFixture<ErrorWindowComponent>;
   let errorHandlingService: ErrorHandlingService;
   let errorModal: ModalWindowComponent;
-  // let errorMessageElement: any;
-  // let errorCommentElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,10 +24,8 @@ describe('ErrorWindowComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ErrorWindowComponent);
     component = fixture.componentInstance;
-    errorHandlingService = fixture.debugElement.injector.get(ErrorHandlingService);
     errorModal = component.errorModal;
-    //let errorMessageElement = fixture.debugElement.query(By.css('.errorMessage'));
-    //console.log('element', errorMessageElement);
+    errorHandlingService = fixture.debugElement.injector.get(ErrorHandlingService);
     fixture.detectChanges();
   });
 
@@ -37,13 +34,31 @@ describe('ErrorWindowComponent', () => {
   });
 
   it('should show modal on error', () => {
+    /* Expect that upon initialization the modal is not visible and that
+    no error message has been bound to the template. */
     expect(errorModal.visible).toBe(false);
+    expect(getElementBySelector<ErrorWindowComponent>(fixture, '.errorMessage')).toBeNull();
 
+    // Simulate the occurrence of an error
     errorHandlingService.handleError(new Error("Test Error"), "Test Message");
+    fixture.detectChanges();
 
+    /* Expect that the modal is visible and the message and comment have been assigned
+    to the appError model and bound to the template. */
     expect(errorModal.visible).toBe(true);
     expect(component.appError.error['message']).toBe("Test Error");
     expect(component.appError.comment).toBe("Test Message");
-    //expect(errorMessageElement.innerText).toBe("Test Error");
+    expect(getElementTextBySelector<ErrorWindowComponent>(fixture, '.errorMessage')).toBe("Test Error");
+    expect(getElementTextBySelector<ErrorWindowComponent>(fixture, '.errorComment')).toBe("Test Message");
   })
+
+  function getElementBySelector<ComponentType>(fixture: ComponentFixture<ComponentType>, selector: string) {
+    const debugElement = fixture.debugElement.query(By.css(selector));
+    return debugElement != null ? debugElement.nativeElement : null;
+  }
+
+  function getElementTextBySelector<ComponentType>(fixture: ComponentFixture<ComponentType>, selector: string) {
+    const nativeElement = getElementBySelector<ComponentType>(fixture, selector);
+    return nativeElement != null ? nativeElement.innerText : null;
+  }
 });
